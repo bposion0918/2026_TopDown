d913824b323d53128e335af09bea3740e6acf77d
bposion0918 <b.posion0918.@gmail.com>
2026-06-03 05:01:27 +0900

    [Fix] 랜덤 스테이지 수정

--- 2026_TopDown/Assets/Script/StageData.cs
+++ 2026_TopDown/Assets/Script/StageData.cs
@@ -5,10 +5,6 @@ public class StageData : ScriptableObject
 {
     public int stageIndex;
 
-    [Header("방 프리팹 풀 (Pool)")]
-    public GameObject startRoomPrefab;      // 시작 방 프리팹
-    public GameObject[] normalRoomPrefabs;  // 일반 방 프리팹들 (여러 개 등록 가능)
-    public GameObject treasureRoomPrefab;   // 보물 방 프리팹
-    public GameObject shopRoomPrefab;       // 상점 방 프리팹
-    public GameObject bossRoomPrefab;       // 보스 방 프리팹
+    [Header("이 스테이지에 나올 수 있는 맵 디자인들")]
+    public GameObject[] mapDesigns;
 }
